8aca4dd860325d6a883d0c2cd9ff05552e30a3d4
Antonio Maroto <antoniomarotoblasco@gmail.com>
2023-01-26 19:04:10 +0100

    Arreglados pequeños fallos de las querys

--- Exercises/Exercise3.sql
+++ Exercises/Exercise3.sql
@@ -4,7 +4,7 @@ SELECT * FROM employees WHERE salary <= 52500;
 
 -- 2) Based on the suppliers table below, select the unique city values that reside in the state of California and order the results in descending order by city:
 
-SELECT city FROM suppliers WHERE state = "California" ORDER BY city DESC;
+SELECT DISTINCT city FROM suppliers WHERE state = "California" ORDER BY city DESC;
 
 -- 3) Based on the customers table and the orders table below, select the customer_id and last_name from the customers table and select the order_date from the orders table where there is a matching customer_id value in both the customers and orders tables. Order the results by customer_id in descending order.
 
@@ -12,9 +12,7 @@ SELECT customers.customer_id, customers.last_name, orders.order_date FROM custom
 
 -- 4) Based on the customers and orders table from Practice Exercise #3, select the customer_id and last_name from the customers table where there is a record in the orders table for that customer_id. Order the results in ascending order by last_name and then descending order by customer_id.
 
-SELECT customers.customer_id, customers.last_name FROM customers, orders WHERE customers.customer_id = orders.customer_id ORDER BY last_name ASC;
-
-SELECT customers.customer_id, customers.last_name FROM customers, orders WHERE customers.customer_id = orders.customer_id ORDER BY customer_id DESC;
+SELECT customers.customer_id, customers.last_name FROM customers, orders WHERE customers.customer_id = orders.customer_id ORDER BY last_name ASC, customer_id DESC;
 
 -- 5) Based on the orders table from Practice Exercise #3, select the distinct customer ids that placed orders in April 2016. Use the BETWEEN keyword.
 
@@ -22,9 +20,9 @@ SELECT DISTINCT customer_id FROM orders where order_date BETWEEN '2016-04-01' AN
 
 -- 6) Based on the customers and orders table from Practice Exercise #3, select the last_name of the customers that placed orders in April, 2016. Use tuple comparison (month(order_date),year(order_date))=(4,2016)
 
-SELECT customers.last_name FROM customers, orders WHERE month(order_date) = (4) AND year(order_date) = (2016);
+SELECT DISTINCT customers.last_name FROM customers, orders WHERE (month(order_date), year(order_date)) = (4,2016);
 
-SELECT DISTINCT customers.last_name FROM customers, orders WHERE month(order_date) = (4) AND year(order_date) = (2016);
+SELECT customers.last_name FROM customers, orders WHERE (month(order_date), year(order_date)) = (4,2016);
 
 -- 7) Based on the instructor table from the university database, compare the following two queries.
 -- select * from instructor where (name = 'Mozart' or dept_name='Music') and salary=100000;
