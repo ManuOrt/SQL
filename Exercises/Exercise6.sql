f1e4d319c028d6130570bb1f2f3fa1423adf415e
Antonio Maroto <antoniomarotoblasco@gmail.com>
2023-01-27 21:07:45 +0100

    Ejercicio 5 del Exercise 6 añadido

--- Exercises/Exercise6.sql
+++ Exercises/Exercise6.sql
@@ -9,15 +9,31 @@ SELECT DISTINCT Person.name FROM Person, Eats WHERE (Person.gender = "female" AN
 
 -- 3) Find the names of all females who eat both mushroom and pepperoni pizza.
 
+(SELECT Person.name FROM Person, Eats WHERE (Person.gender = "female" AND (Eats.pizza = "mushroom")) AND Eats.name = Person.name)
 
+INTERSECT
 
--- 4) Find all pizzerias that serve at least one pizza that Amy eats for less than $10.00.
+(SELECT Person.name FROM Person, Eats WHERE (Person.gender = "female" AND (Eats.pizza = "pepperoni")) AND Eats.name = Person.name);
 
+-- 4) Find all pizzerias that serve at least one pizza that Amy eats for less than $10.00.                    eats SERVES
 
+SELECT Serves.pizzeria FROM Eats, Serves WHERE (Serves.price < 10 AND Eats.name = "Amy") AND Eats.pizza = Serves.pizza;
 
--- 5) Find all pizzerias that are frequented by only females or only males.
+-- 5) Find all pizzerias that are frequented by only females or only males.  Person ⨝ Frequents))
 
+(SELECT Frequents.pizzeria FROM Person, Frequents WHERE Person.gender = "female" AND Person.name = Frequents.name
 
+EXCEPT
+
+SELECT Frequents.pizzeria FROM Person, Frequents WHERE Person.gender = "male" AND Person.name = Frequents.name)
+
+UNION
+
+(SELECT Frequents.pizzeria FROM Person, Frequents WHERE Person.gender = "male" AND Person.name = Frequents.name
+
+EXCEPT
+
+SELECT Frequents.pizzeria FROM Person, Frequents WHERE Person.gender = "female" AND Person.name = Frequents.name);
 
 -- 6) For each person, find all pizzas the person eats that are not served by any pizzeria the person frequents. Return all such person (name) / pizza pairs.
 
