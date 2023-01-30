d8f836fd5f1bee8fc0b2dc91fd4c379f502a69ff
Antonio Maroto <antoniomarotoblasco@gmail.com>
2023-01-30 17:09:07 +0100

    Hecho mayoría del Exercise7

---
+++ Databases/Exercise7.sql
@@ -0,0 +1,60 @@
+DROP DATABASE IF EXISTS Exercise7;
+CREATE DATABASE Exercise7;
+USE Exercise7;
+
+CREATE TABLE salesman(
+    salesman_id INT PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    city VARCHAR(20) NOT NULL,
+    commissiON DECIMAL(6,2) NOT NULL
+);
+
+CREATE TABLE customer(
+    customer_id INT PRIMARY KEY,
+    cust_name VARCHAR(20) NOT NULL,
+    city VARCHAR(20) NOT NULL,
+    grade INT NOT NULL,
+    salesman_id INT,
+    FOREIGN KEY (salesman_id) REFERENCES salesman(salesman_id) ON DELETE SET NULL
+);
+
+CREATE TABLE orders(
+    ord_no INT PRIMARY KEY,
+    pur_amt DECIMAL(7,2) NOT NULL,
+    ord_date DATE NOT NULL,
+    customer_id INT,
+    salesman_id INT,
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON DELETE SET NULL,
+    FOREIGN KEY (salesman_id) REFERENCES salesman(salesman_id) ON DELETE SET NULL
+);
+
+CREATE TABLE student(
+    name VARCHAR(30) PRIMARY KEY,
+    grade INT
+);
+
+
+INSERT INTO salesman VALUES (11,'Pranav','Karwar',200);
+INSERT INTO salesman VALUES (24,'Prasanna','Bengalore',300);
+INSERT INTO salesman VALUES (39,'Prajwal','Kodagu',100);
+INSERT INTO salesman VALUES (44,'Pooja','Hubli',500.5);
+INSERT INTO salesman VALUES (15,'Prokta','Mysore',200.2);
+
+INSERT INTO customer VALUES (101,'Bhargav','Mysore',1,15);
+INSERT INTO customer VALUES (206,'Ramya','Bengalore',3,24);
+INSERT INTO customer VALUES (225,'Rajesh','Hubli',2,39);
+INSERT INTO customer VALUES (324,'Ravi','Mangalore',5,44);
+INSERT INTO customer VALUES (456,'Rajdeep','Belagavi',3,15);
+INSERT INTO customer VALUES (501,'Raghu','Dharavad',4,39);
+INSERT INTO customer VALUES (300,'Bhavya','Bengalore',1,15);
+
+INSERT INTO orders VALUES (5,10000,STR_TO_DATE('25-03-2020','%e-%c-%Y'),101,11);
+INSERT INTO orders VALUES (10,5000,STR_TO_DATE('25-03-2020','%e-%c-%Y'),456,15);
+INSERT INTO orders VALUES (7,9500,STR_TO_DATE('30-04-2020','%e-%c-%Y'),225,44);
+INSERT INTO orders VALUES (11,8700,STR_TO_DATE('07-07-2020','%e-%c-%Y'),324,24);
+INSERT INTO orders VALUES (17,1500,STR_TO_DATE('07-07-2020','%e-%c-%Y'),206,39);
+
+INSERT INTO student VALUES('Chicote', 10);
+INSERT INTO student VALUES('Axe', 0);
+INSERT INTO student VALUES('Doraemon',6);
+INSERT INTO student VALUES('Mathilda', 5);
---
+++ Exercises/Exercise7.sql
@@ -0,0 +1,51 @@
+-- 1) Find all the names of all the salesman and customers.
+
+SELECT cust_name FROM customer
+
+UNION
+
+SELECT name FROM salesman;
+
+-- 2) Find all the cities where there are salesman and all the cities where there are customers. Include duplicates.
+
+SELECT city FROM customer
+
+UNION ALL
+
+SELECT city FROM salesman;
+
+-- 3) Find all the cities where there are salesman and customers.
+
+SELECT city FROM customer
+
+INTERSECT
+
+SELECT city FROM salesman;
+
+-- 4) Find all the customers and salesmen that live in Bengalore. Label the column customers_and_salesmen.
+
+SELECT cust_name FROM customer WHERE city = 'Bengalore'
+
+UNION
+
+SELECT name FROM salesman WHERE city = 'Bengalore';
+
+-- 5) Find all the cities in which there is either a customer or a salesman involved in an order.
+
+SELECT salesman.city FROM salesman, orders WHERE salesman.salesman_id = orders.salesman_id
+
+UNION
+
+SELECT customer.city FROM customer, orders WHERE customer.customer_id = orders.customer_id;
+
+-- 6) Change the output of the grade column to pass if the grade is greater than or equal to 5 or fail if the grade is less than 5.
+
+SELECT name, 'pass' AS result FROM student WHERE grade >= 5
+
+UNION
+
+SELECT name, 'fail' AS result FROM student WHERE grade < 5;
+
+-- 7) Create a problem and (the solution) using set operators.
+
+
